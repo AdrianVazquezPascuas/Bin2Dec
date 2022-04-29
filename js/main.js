553ca99f877898274402e2ec84ec391c3233fd06
Adrián <100007005.joan23@fje.edu>
2022-04-29 16:51:37 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-function decimal() {
+function parrafos() {
+    var infonum = document.getElementById('infonum')
 
+    var infonum2 = document.getElementById('infonum2')
+    var numero = document.getElementById('numero').value
+    var calculando = document.getElementById('calculando')
+    calculando.innerHTML = parseInt(numero, 2);
+    var ultimodigito = numero.charAt(numero.length - 1)
+    if (infonum == 0 || 1) {}
+    if (ultimodigito == 0 || ultimodigito == 1) {
+        infonum2.innerHTML = 'Here is your decimal'
+    } else {
+        calculando.innerHTML = 'Solo 0 y 1'
+    }
 }
