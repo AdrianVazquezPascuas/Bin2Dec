8502641bf8ecbf90638ee5a2385e5d65a0e33995
Adrián <100007005.joan23@fje.edu>
2022-05-04 16:58:52 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-function decimal() {
-    var infonum = document.getElementById('infonum')
-
-    var infonum2 = document.getElementById('infonum2')
-    var numero = document.getElementById('numero').value
-    var calculando = document.getElementById('calculando')
-    calculando.innerHTML = parseInt(numero, 2);
-    var ultimodigito = numero.charAt(numero.length - 1)
-    if (infonum == 0 || 1) {}
-    if (ultimodigito == 0 || ultimodigito == 1) {
-        infonum2.innerHTML = 'Here is your decimal'
-    } else {
-        calculando.innerHTML = 'Solo 0 y 1'
+function calcular() {
+    var infonum = document.getElementById('text1')
+    var infonum2 = document.getElementById('text2')
+    var num = document.getElementById('number').value
+    var calculando = document.getElementById('resultado')
+    var selector = document.getElementById('selector').value
+    var bin = document.getElementById('bin')
+    var decimal = 255;
+    var result = decimal.toString(16);
+    calculando.innerHTML = parseInt(num, 2);
+    var lastnum = num.charAt(num.length - 1)
+    if (num == '') {
+        calculando.innerHTML = ''
     }
+    if (lastnum == 0 || lastnum == 1) {
+        infonum2.innerHTML = 'Here is your decimal!'
+        infonum.innerHTML = ''
+    } else {
+        calculando.innerHTML = 'Waiting for a valid binary number...'
+    }
+    if (lastnum >> 1 || lastnum >> 999999) {
+        infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
+        infonum2.innerHTML = ''
+    }
+    if (lastnum == '') {
+        infonum.innerHTML = ''
+        infonum2.innerHTML = ''
+    }
+
+
+
 }
